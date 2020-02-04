6e0ccd920a295276f31d642a6f174c7f23c17db8
Felipe Barros <felipetech@tuta.io>
2020-02-04 09:31:05 -0300

    Refactoring code

--- utils/typeof.js
+++ utils/typeof.js
@@ -1,8 +1,8 @@
 module.exports = {
   isNumber(element) {
-    return Object.prototype.toString.call(element) === '[object Number]' ? true : false;
+    return Object.prototype.toString.call(element) === '[object Number]';
   },
   isString(element) {
-    return Object.prototype.toString.call(element) === '[object String]' ? true : false;
+    return Object.prototype.toString.call(element) === '[object String]';
   }
 }
