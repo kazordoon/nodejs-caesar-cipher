9ca5efcc2e81f47fd1865ffc13f3e65baaffcebb
Felipe Barros <felipetech@tuta.io>
2020-08-22 00:13:32 -0300

    test: Update the caesarCipher function tests

--- src/caesar-cipher.test.js
+++ src/caesar-cipher.test.js
@@ -1,35 +1,49 @@
 const assert = require('assert');
 const caesarCipher = require('./caesar-cipher');
+const ValidationException = require('./errors/ValidationException');
 
 describe('Caesar Cipher', () => {
   it('should shuffle the word "Hello World"', () => {
     const expected = 'Ifmmp Xpsme';
-    const given = caesarCipher('Hello World');
-    assert.deepStrictEqual(expected, given);
+    const actual = caesarCipher('Hello World');
+    assert.deepStrictEqual(expected, actual);
   });
-  it('should return an warning telling that the text is not a string', () => {
-    const expected = 'The type of the text must be a string.';
-    const given = caesarCipher(true);
-    assert.deepStrictEqual(expected, given);
-  });
-  it('should return an warning telling that the leap is not a number', () => {
-    const expected = 'The type of the leap must be a number';
-    const given = caesarCipher('Hello World', true);
-    assert.deepStrictEqual(expected, given);
-  });
-  it('should return an warning telling that the leap overwrote the number 26', () => {
-    const expected = 'The leap must be greater than 0 and less than 26';
-    const given = caesarCipher('Hello World', 27);
-    assert.deepStrictEqual(expected, given);
-  });
+
   it('should return the word "a" when the word "z" is provided', () => {
     const expected = 'a';
-    const given = caesarCipher('z');
-    assert.deepStrictEqual(expected, given);
+    const actual = caesarCipher('z');
+    assert.deepStrictEqual(expected, actual);
   });
+
   it('should return the word "A" when the word "Z" is provided', () => {
     const expected = 'A';
-    const given = caesarCipher('Z');
-    assert.deepStrictEqual(expected, given);
+    const actual = caesarCipher('Z');
+    assert.deepStrictEqual(expected, actual);
   });
+
+  context('when an validation error occurs', () => {
+    it('should return an exception telling that the text is not a string', () => {
+      const expected = new ValidationException('The type of the text must be a string.');
+      const actual = caesarCipher(true);
+
+      assert.deepStrictEqual(actual instanceof ValidationException, true);
+      assert.deepStrictEqual(expected, actual);
+    });
+  
+    it('should return an exception telling that the leap is not a number', () => {
+      const expected = new ValidationException('The type of the leap must be a number.');
+      const actual = caesarCipher('Hello World', true);
+      
+      assert.deepStrictEqual(actual instanceof ValidationException, true);
+      assert.deepStrictEqual(expected, actual);
+    });
+  
+    it('should return an exception telling that the leap exceeded the number 26', () => {
+      const expected = new ValidationException('The leap must be greater than 0 and less than 26.');
+      const actual = caesarCipher('Hello World', 27);
+      
+      assert.deepStrictEqual(actual instanceof ValidationException, true);
+      assert.deepStrictEqual(expected, actual);
+    });
+  })
 });
