26c9a6120686a8bed2f1508161bea2a035b758dd
Felipe Barros <felipetech@tuta.io>
2020-08-22 00:20:41 -0300

    chore: Remove duplicate utils directory

--- utils/typeof.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  isNumber(element) {
-    return Object.prototype.toString.call(element) === '[object Number]';
-  },
-  isString(element) {
-    return Object.prototype.toString.call(element) === '[object String]';
-  }
-}
